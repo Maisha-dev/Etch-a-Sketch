32b7ae6da262822d408077b3dcfaca7ee61a059c
Hamzat Aishat <hamzataishat2020@gmail.com>
2025-12-16 01:01:34 +0100

    Eventlistener fo each square

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const container = document.querySelector(".container");
 
+const squares = [];
+
 for (let i = 0; i < 256; i++) {
   const squareDiv = document.createElement("div");
 
   squareDiv.classList.add("square");
 
   container.appendChild(squareDiv);
+
+  squares.push(squareDiv);
 }
 
-// squareDiv.addEventListener("mouseenter", function () {
-//   this.classList.add("hovered");
-// });
+squares.forEach((square) => {
+  square.addEventListener("mouseenter", function () {
+    this.classList.add("hovered");
+  });
+});
